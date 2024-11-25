213830e7aad309ec6a1180919e0da8bf08fbb3ce
arpenel <arthurpnlcontact1@gmail.com>
2024-11-25 12:53:02 +0100

    ft-itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,50 +6,59 @@
 /*   By: arpenel <arpenel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 14:34:42 by arpenel           #+#    #+#             */
-/*   Updated: 2024/11/22 16:51:57 by arpenel          ###   ########.fr       */
+/*   Updated: 2024/11/25 12:52:51 by arpenel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 
-int ft_count(int n)
+int	ft_count(int n)
 {
-    size_t  i;
+	size_t	i;
 
-    i = 0;
-    while (n != 0)
-    {
-        n /= 10;
-        i++;
-    }
-    return (i);
+	i = 0;
+	if(n == 0)
+		return (1);
+	while (n != 0)
+	{
+		n /= 10;
+		i++;
+	}
+	return (i);
 }
 
-char    *ft_itoa(int n)
+char	*ft_itoa(int n)
 {
-    char    *res;
-    size_t  count;
-    long int    number;
+	char    *res;
+	size_t  count;
+	long int	number;
 
-    number = n;
-    count = ft_count(n);
-    if (number < 0)
-    {
-        number *= -1;
-        count++;
-    }
+	number = n;
+	count = ft_count(n);
+	if (number < 0)
+	{
+		number *= -1;
+		count++;
+	}
 
-    res = malloc(sizeof(char *) * (count + 1));
-    if (!res)
-        return (NULL);
+	res = malloc(sizeof(char *) * (count + 1));
+	if (!res)
+		return (NULL);
 
-    res[count] = '\0';
+	res[count] = '\0';
 
-    if (n == 0)
-    {
-        res[0] = '0';
-        return (res);
-    }
-
+	if (n == 0)
+	{
+		res[0] = '0';
+		return (res);
+	}
+	while (number > 0)
+	{
+		res[--count] = number % 10 + 48;
+		number /= 10; 
+	}
+	if (n < 0)
+		res[0] = '-';
+	return (res);
 }
