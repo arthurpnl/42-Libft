92fd5422ba08a154619ef9b0974a0c9dd318cccf
arpenel <arthurpnlcontact1@gmail.com>
2024-11-28 18:19:32 +0100

    random save

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,20 +6,20 @@
 /*   By: arpenel <arpenel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 14:17:13 by arpenel           #+#    #+#             */
-/*   Updated: 2024/11/28 15:39:57 by arpenel          ###   ########.fr       */
+/*   Updated: 2024/11/28 15:42:15 by arpenel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list  *ft_lstnew(void *content)
+t_list	*ft_lstnew(void *content)
 {
-    t_list  *new;
+	t_list	*new;
 
-    new = malloc(sizeof(*new));
-    if  (!new)
-        return (NULL);
-    new->content = content;
-    new->next = NULL;
-    return (new);
+	new = malloc(sizeof(*new));
+	if (!new)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
 }
