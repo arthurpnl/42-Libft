cc8eeb266a71aba916e6b830814db57fba76c292
Arthur Penel <arpenel@made-f0Br1s2.clusters.42paris.fr>
2024-12-09 13:35:08 +0100

    split updated + strcmp norm error

--- ft_split.c
+++ ft_split.c
@@ -6,89 +6,64 @@
 /*   By: arpenel <arpenel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 16:28:17 by arpenel           #+#    #+#             */
-/*   Updated: 2024/12/05 16:42:51 by arpenel          ###   ########.fr       */
+/*   Updated: 2024/12/09 13:30:14 by arpenel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	is_it_sep(char c, char sep)
+size_t    ft_countwords(const char *s, char c)
 {
-	if (c == sep)
-		return (1);
-	return (0);
+    size_t    i;
+    size_t    words;
+
+    words = 0;
+    i = 0;
+    while (s[i])
+    {
+        if ((!i || s[i - 1] == c) && s[i] != c)
+            words++;
+        i++;
+    }
+    return (words);
 }
 
-static int	count_words(char *str, char sep)
+void    free_matrix(char **t)
 {
-	size_t	i;
-	size_t	words;
+    size_t    i;
 
-	words = 0;
-	i = 0;
-	while (str[i])
-	{
-		if (is_it_sep(str[i + 1], sep) == 1 && is_it_sep(str[i], sep) == 0)
-			words++;
-		i++;
-	}
-	return (words);
+    i = 0;
+    while (t[i])
+        free(t[i++]);
+    free(t);
 }
 
-int	ft_len_words(const char *str, char sep)
+char    **ft_split(char const *s, char c)
 {
-	size_t	i;
+    size_t    i;
+    size_t    word;
+    char    **ret;
 
-	i = 0;
-	while (str[i] && is_it_sep(str[i], sep) == 0)
-		i++;
-	return (i);
-}
-
-char	*cpy_word(const char	*str, char sep, size_t len)
-{
-	size_t	i;
-	char	*cpy;
-
-	i = 0;
-	cpy = malloc(sizeof(char) * (ft_len_words(str, sep) + 1));
-	if (!cpy)
-		return (NULL);
-	while (str[i] && i < len)
-	{
-		cpy[i] = str[i];
-		i++;
-	}
-	cpy[i] = '\0';
-	return (cpy);
-}
-
-char	**ft_split(const char *s, char sep)
-{
-	char	**res;
-	size_t	i;
-	size_t	j;
-
-	if (!s)
-		return (NULL);
-	res = malloc(sizeof(char *) * (count_words((char *)s, sep) + 1));
-	if (!res)
-		return (NULL);
-	i = 0;
-	j = 0;
-	while (s[i])
-	{
-		if (is_it_sep(s[i], sep) == 1)
-			i++;
-		else
-		{
-			res[j] = cpy_word(&s[i], sep, ft_len_words(&s[i], sep));
-			j++;
-			i = i + ft_len_words(&s[i], sep);
-		}
-	}
-	res[j] = 0;
-	return (res);
+    ret = ft_calloc((ft_countwords(s, c) + 1), sizeof(*ret));
+    if (!ret)
+        return (NULL);
+    word = 0;
+    i = 0;
+    while (*s)
+    {
+        i = 1;
+        if (*s != c)
+        {
+            while (s[i] && s[i] != c)
+                i++;
+            ret[word] = malloc(i + 1);
+            if (!ret[word])
+                return (free_matrix(ret), NULL);
+            ft_strlcpy(ret[word++], s, i + 1);
+        }
+        s += i;
+    }
+    return (ret);
 }
 
 /*
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,13 +6,11 @@
 /*   By: arpenel <arpenel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 17:05:06 by arpenel           #+#    #+#             */
-/*   Updated: 2024/12/02 17:13:27 by arpenel          ###   ########.fr       */
+/*   Updated: 2024/12/09 13:30:49 by arpenel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
